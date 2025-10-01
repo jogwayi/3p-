267f6d7be98c5334358026c669e40157ddbe3437
Josephat <jogwayi@users.noreply.github.com>
2025-10-01 12:29:01 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
+import { Connect3pAppService } from './connect-3p-app.service';
 
 Amplify.configure(outputs);
 
@@ -11,8 +12,12 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  providers: [Connect3pAppService],
   imports: [RouterOutlet, TodosComponent],
 })
 export class AppComponent {
+  constructor(private connect3pAppService: Connect3pAppService) {
+
+  }
   title = 'amplify-angular-template';
 }
